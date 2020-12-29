18f3e67d28dcb4ca04f904c4ddeca431d679348e
jungwoonlim <wypo23@naver.com>
2020-12-29 23:24:02 +0900

    First steps with Typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "jCloud",
+    age = 30,
+    gender = "male";
+
+const sayHello = (name, age, gender?) =>{
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+}
+
+sayHello(name, age);
+
+export {};
